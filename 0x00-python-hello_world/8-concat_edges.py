669e83f143ba3ecf06492eb712f8e6c527f8fedb
Abdellahsyani <syaniabdos@gmail.com>
2023-11-27 15:55:09 +0100

    8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+print(str[39:66], end=" ")
+print("with Python")
