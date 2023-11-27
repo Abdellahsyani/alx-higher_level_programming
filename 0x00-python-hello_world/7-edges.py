7f7bc025b8d64c6c86d4c6a91ea32f9b1b8ec5ad
Abdellahsyani <syaniabdos@gmail.com>
2023-11-27 17:07:33 +0100

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-first = word[0:3]
-second = word[7:]
-middle = word[1:-1]
-print(f"First 3 letters: {first}")
-print(f"Last 2 letters: {second}")
-print(f"Middle word: {middle}")
+word_first_3 = word[:3]
+word_last_2 = word[7:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
