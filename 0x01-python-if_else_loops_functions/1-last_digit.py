abffa2c4dac8a2f96b7153d8c487577edb67b3b9
Abdellahsyani <syaniabdos@gmail.com>
2023-11-28 15:11:41 +0100

    ors mat aoya

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 if number % 10 > 5 or -number % 10 > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, number % 10))
+    a = number % 10
+    x = -number % 10
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, a or x))
 elif number % 10 == 0 or -number % 10 == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, number % 10))
+    x = -number % 10
+    print("Last digit of {:d} is {:d} and is 0".format(number, number % 10 or x))
 elif number % 10 < 6 and not 0 or -number % 10 < 6 and not 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, number % 10))
+    b = number % 10
+    x = -number % 10
+    print("Last digit of", end=" ")
+    print("{:d} is {:d} and is less than 6 and not 0".format(number, b or x))
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+alphabet = ""
+for i in range(97, 123):
+    alphabet += "{:s}".format(chr(i))
+print(alphabet, end=" ")
