ed4d67e64d3d165e8431f54e2e422958be5769f6
Abdellahsyani <syaniabdos@gmail.com>
2023-11-29 17:47:59 +0100

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 ''' authur abdellah'''
 value = 0
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(i - value)), end="")
+    print("{}".format(chr(i + value)), end="")
     value = 32 if i == 0 else 0
---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,3 @@
+#!/usr/bin/puthon3
+def magic_calculation(a, b, c):
+    return (98 + a ** b ** c)
