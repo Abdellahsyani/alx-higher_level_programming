a5a6a53361b8a653d412f0fcd42703b749e412cb
Abdellahsyani <syaniabdos@gmail.com>
2023-11-30 20:51:00 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     '''the function to print all hidden names'''
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if  not name.startswitch("__"):
-            print(name)
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print("{:s}".format(s))
