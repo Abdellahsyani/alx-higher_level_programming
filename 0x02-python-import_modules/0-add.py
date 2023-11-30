cce753372994dafbc48a2fcdd270847ae14ad87b
Abdellahsyani <syaniabdos@gmail.com>
2023-11-30 10:37:04 +0100

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''authur abdellah syani'''
 from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
