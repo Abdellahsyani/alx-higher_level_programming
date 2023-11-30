95f5312c590a2b6a5869e49f88ce3e65b2f42262
Abdellahsyani <syaniabdos@gmail.com>
2023-11-30 12:45:53 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import sys
-sys.argv
-num_args = len(sys.argv)
-
-if num_args == 0:
-    print("{} arguments.".format(0))
-if num_args > 0:
-    print("{} argument:".format(num_args))
-if num_args > 0:
+if __name__ == "__main__":
+    '''splite the list'''
+    import sys
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("{} arguments.".format(0))
+    elif num_args == 1:
+        print("{} argument:".format(1))
+    else:
+        print("{} arguments:".format(num_args))
     for i in range(num_args):
-        print("{}: {}".format(i, sys.argv[i]))
-        i + 1
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
