907da7b79db55b105bf648778bb212cec689ec37
Abdellahsyani <syaniabdos@gmail.com>
2023-12-03 13:35:12 +0100

    def replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if my_list < 0 or my_list >= len(my_list) - 1:
+        return my_list
+    else:
+        return my_list(idx, element)
