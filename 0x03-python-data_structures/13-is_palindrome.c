a1a078f7d3251a95074a5b7ec8f60118b59bc58f
Abdellahsyani <syaniabdos@gmail.com>
2023-12-05 11:44:57 +0100

    is_apalindrom

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,11 +9,11 @@
 
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL)
+	listint_t *fast = *head, *slow = *head;
+	listint_t *prev = NULL;
+      	listint_t *next = NULL;
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	listint_t *fast = *head;
-	listint_t *slow = *head;
-	listint_t *prev = NULL, *next = NULL;
 
 	while (fast != NULL && fast->next != NULL)
 	{
@@ -22,15 +22,13 @@ int is_palindrome(listint_t **head)
 		slow->next = prev;
 		prev = slow;
 		slow = next;
-		if (fast != NULL)
-		{
-			slow = slow->next;
-		}
 	}
 	while (prev != NULL && slow != NULL)
 	{
 		if (prev->n != slow->n)
+		{
 			return (0);
+		}
 		prev = prev->next;
 		slow = slow->next;
 	}
