d5ab233160d771a55cabeed3afecb78c01cb011a
Abdellahsyani <syaniabdos@gmail.com>
2023-12-06 21:29:54 +0100

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,4 +2,7 @@
 def multiply_by_2(a_dictionary):
     new_dictionary = sorted(a_dictionary)
     for key in new_dictionary:
+        print("{}: {}".format(key, a_dictionary[key]))
+    print('--')
+    for key in new_dictionary:
         print("{}: {}".format(key, a_dictionary[key] * 2))
