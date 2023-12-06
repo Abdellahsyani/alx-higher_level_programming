510d09e47c9e80bdc7e793f1199385f1be2bbd3d
Abdellahsyani <syaniabdos@gmail.com>
2023-12-06 15:59:28 +0100

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_set = set{}
+    result = 0
+    for i in my_list:
+        if i not in new_set:
+            new_set.add(i)
+            result += i
+    return result
