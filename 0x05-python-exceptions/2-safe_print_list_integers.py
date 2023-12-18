9dbb2cc69c69eddfc1527492e8cb49c9659abf6e
Abdellahsyani <syaniabdos@gmail.com>
2023-12-18 23:34:05 +0100

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             ret += 1
         except (ValueError, TypeError):
             continue
-        print("")
-        return (ret)
+    print("")
+    return (ret)
