0fba479fb042a3d8509f224bf81c3af6a97fd112
Abdellahsyani <syaniabdos@gmail.com>
2023-12-20 21:34:57 +0100

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -13,9 +13,11 @@ class Square:
 
     @property
     def size(self):
+        """the property function to has an attrbute"""
         return (self.__size)
 
     @size.setter
+    """The setter function to set the value"""
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -24,9 +26,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        """The area funtion to return the size"""
         return (self.__size * self.__size)
 
     def my_print(self):
+        """return the multiplacation size"""
         if self.__size == 0:
             print()
         for i in range(self.__size):
