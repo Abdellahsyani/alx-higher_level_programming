eced9eddf7a6ef4d4f434f5af4fa0ad207aa7428
Abdellahsyani <syaniabdos@gmail.com>
2023-12-20 21:40:56 +0100

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -33,6 +33,7 @@ class Square:
     def my_print(self):
         """return the multiplacation size"""
         for i in range(0, self.__size):
-            print("#" * self.__size)
+            [print("#", end="") for j in range(self.__size)]
+            print("")
         if self.__size == 0:
             print("")
