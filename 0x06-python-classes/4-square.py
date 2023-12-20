a8537b4b73fdbb2c5208056417a8efcd7be39563
Abdellahsyani <syaniabdos@gmail.com>
2023-12-20 13:05:13 +0100

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,7 +2,11 @@
 
 
 class Square:
+    """request the class"""
     def __init__(self, size=0):
+        """the unit funtion
+        @sefl.__size: the size of class
+        """
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -11,14 +15,19 @@ class Square:
 
     @property
     def size(self):
+        """The property function
+        return the size
+    """
         return sefl.__size
 
     @size.setter
     def size(self, value):
+        """the setter function to set value"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """the area function to be attrbute"""
         return self.__size
