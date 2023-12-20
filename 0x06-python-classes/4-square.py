da6de00dab89f186357ff9bf4a0c19831dc66cdc
Abdellahsyani <syaniabdos@gmail.com>
2023-12-20 13:17:12 +0100

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,7 +16,7 @@ class Square:
     @property
     def size(self):
         """The property function"""
-        return (sefl.__size)
+        return (self.__size)
 
     @size.setter
     def size(self, value):
