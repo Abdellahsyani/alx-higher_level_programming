efad43595f1165fc194d9057d186cdeeded4534b
Abdellahsyani <syaniabdos@gmail.com>
2024-01-09 11:11:50 +0100

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,16 +5,3 @@
 def lookup(obj):
     '''the function started'''
     return (dir(obj))
-
-
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
