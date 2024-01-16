2915d2795fb63e3115230e763c29322a1bdc5a95
Abdellahsyani <syaniabdos@gmail.com>
2024-01-16 10:29:59 +0100

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,10 +18,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+    
+    @staticmethod
     def to_json_string(list_dictionaries):
         """method fromat to sharing data representation"""
+        dic = json.dumps(list_dictionaries)
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
-            return json.dumps(list_dictionaries)
+            return dic
