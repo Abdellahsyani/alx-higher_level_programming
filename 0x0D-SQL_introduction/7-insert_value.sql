fd8a70d82fad3c1e800ac8ae30a84b51ebdd637f
Abdellahsyani <syaniabdos@gmail.com>
2024-02-13 19:16:30 +0100

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,6 @@
 -- usage: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 -- insert a new row to table
-INSERT INTO first_table (id, name) VALUES (89, Best School);
+INSERT
+   INTO first_table
+	(id, name)
+VALUES (89, 'Best School')
