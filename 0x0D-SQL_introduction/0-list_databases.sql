b349159de9c42a2e41a47db5b0d8911914dc6ddb
Abdellahsyani <syaniabdos@gmail.com>
2024-02-13 17:28:21 +0100

    0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
---usage cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
+-- usage cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
 -- list available databases in the system
 SHOW databases;
