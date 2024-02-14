19955b23577e55f2c78633cd1595a28600c01d10
Abdellahsyani <syaniabdos@gmail.com>
2024-02-14 21:14:06 +0100

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- 6-states.sql | mysql -hlocalhost -uroot -p
 -- create databases and table
-CREATE IF NOT EXISTS DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256)
