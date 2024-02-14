5ce374708bb51e7aa5b29f452ff23f5a02999496
Abdellahsyani <syaniabdos@gmail.com>
2024-02-14 21:56:23 +0100

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- usage: 8-cities_of_california_subquery.sql | mysql -hlocalhost -uroot -p hb
 -- lists all the cities of california
-SELECT name
-FROM hbtn_0d_usa.cities
-WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California')
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
