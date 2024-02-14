b2c19e5e4e1f6bab91946c9406cee3676a497143
Abdellahsyani <syaniabdos@gmail.com>
2024-02-14 20:53:57 +0100

    3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- 3-force_name.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
 -- creates the table force_name
-CREATE IF NOT EXISTS force_name (
+CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
 	name VARCHAR(256) NOT NULL
 );
