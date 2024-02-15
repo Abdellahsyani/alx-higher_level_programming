212334d9ba5322d0a1937b5e48d90c1a78080590
Abdellahsyani <syaniabdos@gmail.com>
2024-02-15 11:36:22 +0100

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,8 @@
 -- import database dump
 -- lists all shows contained
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows RIGHT JOIN tv_shows_genres
+FROM tv_shows LEFT JOIN tv_shows_genres
 ON tv_showa.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_showa.title ASC,
 	tv_show_genres.genre_id ASC;
