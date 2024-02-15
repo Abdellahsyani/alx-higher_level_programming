09f1b510ece2710f50c60415dc0814102477bcdc
Abdellahsyani <syaniabdos@gmail.com>
2024-02-15 11:39:32 +0100

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,4 +5,4 @@ FROM tv_shows
 LEFT JOIN tv_shows_genres
 ON tv_showa.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_showa.title ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
