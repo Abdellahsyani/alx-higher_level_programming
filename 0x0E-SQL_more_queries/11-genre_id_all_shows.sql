f379c788d9ffe96b6069ce940c2cc340d8357510
Abdellahsyani <syaniabdos@gmail.com>
2024-02-15 11:24:06 +0100

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,6 @@
 -- usage: 11-genre_id_all_shows.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvsh
 -- lists all shows contained
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows LEFT JOIN tv_show_genres 
+ON tv_shows_genres.show_id = tv_shows.id 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
