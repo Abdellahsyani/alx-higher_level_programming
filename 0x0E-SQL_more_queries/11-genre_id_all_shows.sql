13774f68f1a73de1f55e059c5cb845b7ca7baeee
Abdellahsyani <syaniabdos@gmail.com>
2024-02-15 11:22:13 +0100

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,4 +3,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres
 ON tv_shows_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
