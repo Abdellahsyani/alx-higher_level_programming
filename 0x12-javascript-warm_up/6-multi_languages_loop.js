0f8e346154b5428233a8bec6e05c297cbcb3484b
Abdellahsyani <syaniabdos@gmail.com>
2024-03-11 21:32:26 +0000

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* print lines by using array */
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
