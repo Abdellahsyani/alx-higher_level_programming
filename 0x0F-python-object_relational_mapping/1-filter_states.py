d375bfffd66facd7a73aa3dcdd9dd3a2c515e239
Abdellahsyani <syaniabdos@gmail.com>
2024-03-15 15:04:54 +0000

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,10 +7,11 @@ from sys import argv
 if __name__=="__main__":
     conn = MySQLdb.connect(
             host="localhost", port=3306, charset="utf8",
-            user=argv[1], passwd=argv[2], db=argv[3])
+            user=argv[1], passwd=argv[2], db=argv[3]
+            )
     cur = conn.cursor()
     cur.execute("SELECT * FROM states hbtn_0e_0_usa
-            WHERE UPPER(name) LIKE N%
+            WHERE name LIKE N%
             ORDER BY id ASC")
     query = cur.fetchall()
     for row in query:
