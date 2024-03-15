f4d965bf526be615cdbc774fe4690f0240d5d8be
Abdellahsyani <syaniabdos@gmail.com>
2024-03-15 15:07:48 +0000

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,8 @@ if __name__=="__main__":
             user=argv[1], passwd=argv[2], db=argv[3]
             )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states hbtn_0e_0_usa
-            WHERE name LIKE N%
+    cur.execute("SELECT * FROM states\
+            WHERE name LIKE N%\
             ORDER BY id ASC")
     query = cur.fetchall()
     for row in query:
