9e989cc41facc09bbc79659ea1c13a724beb80a6
Abdellahsyani <syaniabdos@gmail.com>
2024-03-15 15:01:47 +0000

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,12 +2,12 @@
 """list all states that's start with n capital"""
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__=="__main__":
     conn = MySQLdb.connect(
             host="localhost", port=3306, charset="utf8",
-            user=argv[0], passwd=argv[1], db="hbtn_0e_0_usa")
+            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states hbtn_0e_0_usa
             WHERE UPPER(name) LIKE N%
