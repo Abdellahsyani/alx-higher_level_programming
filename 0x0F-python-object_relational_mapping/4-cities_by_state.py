966e9349f982e193ffd061ef0cc40babb2b2b0c8
Abdellahsyani <syaniabdos@gmail.com>
2024-03-16 14:47:05 +0000

    fixing one checks fail

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,9 +4,11 @@
 import MySQLdb
 from sys import argv
 
-if __name__=="__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-            user=argv[1], passwd=argv[2], database=argv[3])
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost", port=3306, charset="utf8",
+            user=argv[1], passwd=argv[2], database=argv[3]
+    )
 
     cur = conn.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name\
