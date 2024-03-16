c993c813e418c4d2b8514d908ad7a3ead3a730f6
Abdellahsyani <syaniabdos@gmail.com>
2024-03-16 13:00:41 +0000

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,8 +4,7 @@
 import MySQLdb
 from sys import argv
 
-if __name__=="__main__" and len(argv) == 5:
-    #state = argv[4].split(';')[0]
+if __name__=="__main__":
     conn = MySQLdb.connect(
             host="localhost", port=3306, charset="utf8",
             user=argv[1], passwd=argv[2], db=argv[3]
@@ -15,7 +14,6 @@ if __name__=="__main__" and len(argv) == 5:
     cur.execute(sql, ('%' + argv[4] + '%',))
     query = cur.fetchall()
     for row in query:
-        if row[1] == argv[4]:
-            print(row)
+        print(row)
     cur.close()
     conn.close()
