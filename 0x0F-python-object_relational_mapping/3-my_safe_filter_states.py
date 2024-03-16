9ebd3781fbb988af825cc74bcc863c5b675137dd
Abdellahsyani <syaniabdos@gmail.com>
2024-03-16 12:24:42 +0000

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""usage: use the SQL injection"""
+
+import MySQLdb
+from sys import argv
+
+if __name__=="__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+            user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    sql = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    cur.execute(sql, ('%' + argv[4] + '%',))
+    query = cur.fetchall()
+    for row in query:
+        print(row)
+    cur.close()
+    conn.close()
